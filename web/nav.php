0ee33719785f19fbf9747fba963d4fc109a14e32
sakshianand <sakku.anand@gmail.com>
2017-03-21 22:37:18 +0530

    modifications

---
+++ web/nav.php
@@ -0,0 +1,78 @@
+<div class="header-top">
+		<div class="container">
+			<div class="header-top-in">
+				
+				<ul class="support">
+					<li ><a href="mailto:sakshi.anand2014@vit.ac.in" ><i > </i>sakshi.anand2014@vit.ac.in</a></li>
+					<li ><span ><i class="tele-in"> </i>0 462 261 61 61</span></li>			
+				</ul>
+				<ul class=" support-right">
+				 <li ><a href="account.html" ><i class="men"> </i>Login as customer</a></li>
+				 <li ><a href="account.html" ><i class="men"> </i>Login as seller</a></li>
+				 <li ><a href="account.html" ><i class="tele"> </i>Signup as customer</a></li>	
+				 <li ><a href="account.html" ><i class="tele"> </i>Signup as seller</a></li>			
+				</ul>
+				<div class="clearfix"> </div>
+			</div>
+			</div>
+			<div class="header-bottom">
+			<div class="container">			
+				<div class="logo">
+					<h1><a href="index.html">VIT Emart</a></h1>
+				</div>
+<div class="top-nav">
+				<!-- start header menu -->
+		<ul class="megamenu skyblue">
+			<li><a  href="index.html">Home</a></li>
+				
+				<li><a  href="product.html">Products</a>
+				
+				</li>
+				
+				<li><a  href="contact.html">Contact</a>
+					
+				</li>
+		 </ul> 
+		 <!---->
+		 <div class="search-in" >
+			<div class="search" >
+						<form action="search.html">
+							<input type="text" value="Keywords" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Keywords';}" class="text">
+							<input type="submit" value="SEARCH">
+						</form>
+							<div class="close-in"><img src="images/close.png" alt="" /></div>
+					</div>
+						<div class="right"><button> </button></div>
+				</div>
+						<script type="text/javascript">
+							$('.search').hide();
+							$('button').click(function (){
+							$('.search').show();
+							$('.text').focus();
+							}
+							);
+							$('.close-in').click(function(){
+							$('.search').hide();
+							});
+						</script>
+
+					<!---->
+					<div class="cart box_1">
+						<a href="checkout.html">
+						<h3> <div class="total">
+							<span class="simpleCart_total"></span> (<span id="simpleCart_quantity" class="simpleCart_quantity"></span> items)</div>
+							<img src="images/cart.png" alt=""/></h3>
+						</a>
+						<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+						<div class="clearfix"> </div>
+					</div>
+
+<div class="clearfix"> </div>
+					<!---->
+				</div>
+
+
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		</div>
